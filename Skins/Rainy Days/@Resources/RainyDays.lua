cdc32767ca3fb9f5061e19ee43cafe502e94a5ec
alatsombath <alatsombath@users.noreply.github.com>
2015-04-04 17:23:51 -0700

    1.2 (04 April 2015)
    - Improved visual effects through gradient color and angle adjustments -
    Thanks Ragingwasabi!
    - More friendlier documentation in Variables.inc
    - Increased default Update frequency (42 -> 32)
    - Decreased default RainSpeed (200 -> 100)
    - Removed vertical bar gap variable
    - Removed loading layout
    - Annotated Lua script
    - Fixed file handle leak and starting iteration index when writing
    repetitive measure/meter sections in RepeatSection.lua

--- Skins/Rainy Days/@Resources/RainyDays.lua
+++ Skins/Rainy Days/@Resources/RainyDays.lua
@@ -1,25 +1,60 @@
--- RainyDays v1.1
+-- RainyDays v1.2
 -- LICENSE: Creative Commons Attribution-Non-Commercial-Share Alike 3.0
 
 local HeightLookup,PositionY,Measure,Meter={},{},{},{}
+
 function Initialize()
-	RainSpeed,RainConstant,RainCover=SKIN:ParseFormula(SKIN:ReplaceVariables("#RainSpeed#")),SKIN:ParseFormula(SKIN:ReplaceVariables("#RainConstant#")),SKIN:ParseFormula(SKIN:ReplaceVariables("#RainCover#"))
-	Sub,Index,Limit=SELF:GetOption("Sub"),SKIN:ParseFormula(SELF:GetOption("Index")),SKIN:ParseFormula(SELF:GetOption("Limit"))
+
+	RainSpeed,RainConstant=SKIN:ParseFormula(SKIN:ReplaceVariables("#RainSpeed#")),SKIN:ParseFormula(SKIN:ReplaceVariables("#RainConstant#"))
 	
-	RainDivider,BarHeight=(RainCover*0.5)-(RainCover*0.5)%1,SKIN:ParseFormula(SKIN:ReplaceVariables("#BarHeight#"))
+	-- Iteration control variables
+	Sub,Index,Limit=SELF:GetOption("Sub"),SKIN:ParseFormula(SELF:GetNumberOption("Index")),SKIN:ParseFormula(SELF:GetOption("Limit"))
+	
+	-- Rainfall height (rounded for indexing)
+	RainCover=SKIN:ParseFormula(SKIN:ReplaceVariables("#RainCover#"))
+	
+	-- Raindrop height
+	BarHeight=SKIN:ParseFormula(SKIN:ReplaceVariables("#BarHeight#"))
+	
+	-- Establish halfway point as half the skin height (rounded for indexing)
+	RainDivider=math.ceil(RainCover*0.5)
+	
+	-- Generate height lookup table
 	for i=Index,RainDivider do HeightLookup[i]=BarHeight*(i/RainDivider) end
 	
-	local MeasureName,MeterName,gsub=SKIN:ReplaceVariables("#MeasureName#"),SKIN:ReplaceVariables("#MeterName#"),string.gsub
-	for i=Index,Limit do PositionY[i],Measure[i],Meter[i]=0,SKIN:GetMeasure((gsub(MeasureName,Sub,i))),(gsub(MeterName,Sub,i)) end end
-	
-function Update()
+	-- Retrieve measures and meter names, store in tables
+	local MeasureName,MeterName,gsub=SELF:GetOption("MeasureName"),SELF:GetOption("MeterName"),string.gsub
 	for i=Index,Limit do
-		PositionY[i]=(PositionY[i]+RainSpeed*Measure[i]:GetValue()+RainConstant)%RainCover+1
+		PositionY[i],Measure[i],Meter[i]=0,SKIN:GetMeasure((gsub(MeasureName,Sub,i))),(gsub(MeterName,Sub,i))
+	end
+
+end
+
+function Update()
+
+	-- For each raindrop
+	for i=Index,Limit do
+		
+		-- Increase the Y position of the raindrop based on the measure value and control variables
+		-- Reset when it reaches the bottom of the skin
+		PositionY[i]=(PositionY[i]+RainSpeed*Measure[i]:GetValue()+RainConstant)%RainCover
+		
+		-- Cache the Y position (rounded for indexing)
 		local PositionY=PositionY[i]-PositionY[i]%1
 		
-		if PositionY<=RainDivider then SKIN:Bang("!SetOption",Meter[i],"H",HeightLookup[PositionY])
-		else SKIN:Bang("!SetOption",Meter[i],"H",BarHeight-HeightLookup[PositionY-RainDivider]) end
+		-- Check for invalid index
+		if PositionY==0 then PositionY=1 end
+		
+		-- Set the raindrop height based on whether it has crossed the halfway point
+		if PositionY<=RainDivider then
+			SKIN:Bang("!SetOption",Meter[i],"H",HeightLookup[PositionY])
+		else
+			SKIN:Bang("!SetOption",Meter[i],"H",BarHeight-HeightLookup[PositionY-RainDivider])
+		end
 
+		-- Move the Y position of the meter
 		SKIN:Bang("!SetOption",Meter[i],"Y",PositionY)
+		
 	end
+	
 end
