4eb6773e88380d4470075bd936a8f289f1e43ab8
alatsombath <alatsombath@users.noreply.github.com>
2015-04-04 17:22:11 -0700

    1.1 (23 March 2015)
    - Performance considerations by decreasing Update frequency (16 -> 42)
    and number of Bands (128 -> 64)
    - Increased BarGapX (12R -> 24R)
    - Added a local lookup table for raindrop height

--- Skins/Rainy Days/@Resources/RainyDays.lua
+++ Skins/Rainy Days/@Resources/RainyDays.lua
@@ -1,21 +1,25 @@
--- RainyDays v1.0
+-- RainyDays v1.1
 -- LICENSE: Creative Commons Attribution-Non-Commercial-Share Alike 3.0
 
-local Measure,Meter,PositionY={},{},{}
+local HeightLookup,PositionY,Measure,Meter={},{},{},{}
 function Initialize()
-	RainSpeed,RainCover=SKIN:ReplaceVariables("#RainSpeed#"),SKIN:ParseFormula(SKIN:ReplaceVariables("#RainCover#"))
+	RainSpeed,RainConstant,RainCover=SKIN:ParseFormula(SKIN:ReplaceVariables("#RainSpeed#")),SKIN:ParseFormula(SKIN:ReplaceVariables("#RainConstant#")),SKIN:ParseFormula(SKIN:ReplaceVariables("#RainCover#"))
 	Sub,Index,Limit=SELF:GetOption("Sub"),SKIN:ParseFormula(SELF:GetOption("Index")),SKIN:ParseFormula(SELF:GetOption("Limit"))
+	
+	RainDivider,BarHeight=(RainCover*0.5)-(RainCover*0.5)%1,SKIN:ParseFormula(SKIN:ReplaceVariables("#BarHeight#"))
+	for i=Index,RainDivider do HeightLookup[i]=BarHeight*(i/RainDivider) end
+	
 	local MeasureName,MeterName,gsub=SKIN:ReplaceVariables("#MeasureName#"),SKIN:ReplaceVariables("#MeterName#"),string.gsub
-	for i=Index,Limit do Measure[i]=SKIN:GetMeasure((gsub(MeasureName,Sub,i))) Meter[i]=(gsub(MeterName,Sub,i)) PositionY[i]=0 end end
-
+	for i=Index,Limit do PositionY[i],Measure[i],Meter[i]=0,SKIN:GetMeasure((gsub(MeasureName,Sub,i))),(gsub(MeterName,Sub,i)) end end
+	
 function Update()
-	for i=Index,Limit do 
-		PositionY[i]=(PositionY[i]+RainSpeed*Measure[i]:GetValue()+SKIN:ReplaceVariables("#RainConstant#"))%RainCover
+	for i=Index,Limit do
+		PositionY[i]=(PositionY[i]+RainSpeed*Measure[i]:GetValue()+RainConstant)%RainCover+1
+		local PositionY=PositionY[i]-PositionY[i]%1
 		
-		local Height=PositionY[i]/(RainCover*0.5)
-		if Height>1 then Height=(1/Height) end
-		
-		SKIN:Bang("!SetOption",Meter[i],"H",SKIN:ParseFormula(SKIN:ReplaceVariables("#BarHeight#"))*Height)
-		SKIN:Bang("!SetOption",Meter[i],"Y",PositionY[i])
+		if PositionY<=RainDivider then SKIN:Bang("!SetOption",Meter[i],"H",HeightLookup[PositionY])
+		else SKIN:Bang("!SetOption",Meter[i],"H",BarHeight-HeightLookup[PositionY-RainDivider]) end
+
+		SKIN:Bang("!SetOption",Meter[i],"Y",PositionY)
 	end
 end
